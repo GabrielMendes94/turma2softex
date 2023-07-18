be0ee724f5c927c2dd05787a0232186e9f8d3eff
Gabriel Mendes <103916287+GabrielMendes94@users.noreply.github.com>
2023-07-18 18:55:08 +0000

    testando comando console

--- aula3.js
+++ aula3.js
@@ -1,2 +1,11 @@
-console.log("Hello World");
+console.log("======== MENU ========");
+console.log("1 - Listar Clientes");
+console.log("2 - Cadastrar Cliente");
+console.log("3 - Editar Cliente");
+console.log("4 - Remover Cliente");
+console.log("0 - Sair do Sistema");
+console.log("======================");
+console.log("Opção: __");
+console.log("======================");
+
 // alert ("teste")
