d4129a905c2b297298f718a92aedfede92706460
Gabriel Mendes <103916287+GabrielMendes94@users.noreply.github.com>
2023-08-17 19:32:22 +0000

    Exceptions

--- aula11.js
+++ aula11.js
@@ -4,48 +4,48 @@ Divisão por zero" no console.*/
 
 const readline = require("readline-sync");
 
-// try {
-//     const numerador = readline.questionInt("Digite o numerador: ")
-//     const divisor = readline.questionInt("Digite o divisor: ")
+try {
+    const numerador = readline.questionInt("Digite o numerador: ")
+    const divisor = readline.questionInt("Digite o divisor: ")
 
-//     if (divisor === 0) {
+    if (divisor === 0) {
 
-//         throw "Erro";
+        throw "Erro";
 
-//     }
+    }
 
-//     const resultado = numerador / divisor;
+    const resultado = numerador / divisor;
 
-//     console.log("O resultado é:", resultado);
+    console.log("O resultado é:", resultado);
 
-// } catch (error) {
+} catch (error) {
 
-//     console.error("Erro: Divisão por zero");
+    console.error("Erro: Divisão por zero");
 
-// }
+}
 
 
 /*2.Crie um programa que recebe do teclado uma string não numérica em um
 número utilizando parseInt dentro de um bloco try...catch. Capture o erro
 e o exiba "Erro: Conversão inválida" no console.*/
 
-// try {
+try {
 
-//     let str = readline.question("Digite uma string: ");
-//     console.log(typeof str);
+    let str = readline.question("Digite uma string: ");
+    console.log(typeof str);
 
-//     if (isNaN(str)){
-//         throw "Erro";
-//     }
+    if (isNaN(str)){
+        throw "Erro";
+    }
 
-//     const num = parseInt(str);
-//     console.log(typeof num);
+    const num = parseInt(str);
+    console.log(typeof num);
 
-//     console.log(`Funcionou: ${num}`);
+    console.log(`Funcionou: ${num}`);
 
-// } catch (error) {
-//     console.error("Erro: Conversão inválida");
-// }
+} catch (error) {
+    console.error("Erro: Conversão inválida");
+}
 
 /*3.Crie um programa que recebe do teclado um objeto e tente acessar uma
 propriedade inexistente desse objeto dentro de um bloco try...catch.
@@ -59,7 +59,7 @@ try {
 
     console.log(pessoa.nome);
     console.log(pessoa.idade);
-    
+
     if (pessoa.altura == undefined) {
         throw "Erro";
     }
